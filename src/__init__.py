e557254925bb7c1207d1f7170186d5650ee8c180
Sunny Pepple <slpepple01@gmail.com>
2024-05-05 12:02:56 -0500

    Connection to Postgres database has been extablished

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,25 @@
 from flask import Flask, request
-
+from sqlalchemy.orm import DeclarativeBase
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from src.helper.Helper import Helper
 from datetime import timedelta
 from dotenv import dotenv_values
 
 app = Flask(__name__)
+
 # connection to db
 ENV_CONFIG = dotenv_values(".env")
+ACCESS_EXPIRES= 30
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = ENV_CONFIG['DATABASE_URL']
+
+app.config.update({
+    "TESTING": True,
+    "TEMPLATES_AUTO_RELOAD":True
+})
+
 helper = Helper()
 
 from src.routes.user_route import user_route
